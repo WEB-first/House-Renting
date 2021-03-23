88a1ed1b18f2e7df1da7d674dde5a74e31675517
ruanweina <1191634122@qq.com>
2021-03-24 00:21:04 +0800

    xiugai re

---
+++ src/views/recommend.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="recommend">
+    <mt-navbar fixed v-model="navactive">
+      <mt-tab-item id="1">综合</mt-tab-item>
+      <mt-tab-item id="2">二手房</mt-tab-item>
+      <mt-tab-item id="3">租房</mt-tab-item>
+      <mt-tab-item id="4">新房</mt-tab-item>
+    </mt-navbar>
+    <mt-tab-container v-model="navactive" style="margin-top:50px">
+      <mt-tab-container-item id="1">
+        <recom-list></recom-list>   
+      </mt-tab-container-item>
+      <mt-tab-container-item id="2">
+        二手房
+       </mt-tab-container-item>
+      <mt-tab-container-item id="3">
+        租房
+      </mt-tab-container-item>
+      <mt-tab-container-item id="4">
+        新房
+      </mt-tab-container-item>
+    </mt-tab-container>
+  </div>
+</template>
+<style>
+  .mint-navbar .mint-tab-item.is-selected {
+    border-bottom: 3px solid cadetblue;
+    color: cadetblue;
+    font-weight: bold;
+}
+</style>
+<script>
+//引入的Header组件
+import RecomList from '../components/RecomList.vue'
+export default {
+  components: { RecomList },
+  data(){
+    return{
+       navactive : '1'
+    }
+  }
+}
+</script>
