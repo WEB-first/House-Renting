9077b211a96c61f2aae2c919dd3fcf80b1420ae2
xiaohei <1974551574@qq.com>
2021-03-25 13:56:09 +0800

    detail

--- src/router/index.js
+++ src/router/index.js
@@ -1,103 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Mine from '../views/Mine.vue'
 import Detail from '../views/Detail.vue'
-import Article from '../views/Article.vue'
-import Recommend from '../views/Recommend.vue'
-import Info from '../components/Info.vue'
-import Info_more from '../components/Info_more.vue'
-import Info_more_add from '../components/Info_more_add.vue'
-import Info_more_email from '../components/Info_more_email.vue'
-import Info_more_sex from '../components/Info_more_sex.vue'
-import Info_more_phone from '../components/Info_more_phone.vue'
-import Info_updatepwd from '../components/Info_updatepwd.vue'
-import Info_updatepwd_forgetpwd from '../components/Info_updatepwd_forgetpwd.vue'
-import Info_uname from '../components/Info_uname.vue'
-import Info_mingzi from '../components/Info_mingzi.vue'
-
+import Talk from '../views/Talk.vue'
+import Phone from '../views/Phone.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/article',
-    name: 'Article',
-    component: Article
-  },
-  ,
-  {
-    path: '/article',
-    name: 'Article',
-    component: Article
+    path: '/Phone',
+    name: 'Phone',
+    component:Phone
   },
   {
-    path: '/detail',
-    name: 'Detail',
-    component: Detail
+    path: '/talk',
+    name: 'Talk',
+    component: Talk
   },
   {
-    path: '/recommend',
-    name: 'Recommend',
-    component: Recommend
-  },
-  {
-    path: '/info_uname',
-    name: 'Info_uname',
-    component: Info_uname
-  },
-  {
-    path: '/info_mingzi',
-    name: 'Info_mingzi',
-    component: Info_mingzi
-  },
-  {
-    path: '/info_updatepwd_forgetpwd',
-    name: 'Info_updatepwd_forgetpwd',
-    component: Info_updatepwd_forgetpwd
-  },
-  {
-    path: '/info_updatepwd',
-    name: 'Info_updatepwd',
-    component: Info_updatepwd
-  },
-  {
-    path: '/info_more_email',
-    name: 'Info_more_email',
-    component: Info_more_email
-  },
-  {
-    path: '/info_more_phone',
-    name: 'Info_more_phone',
-    component: Info_more_phone
-  },
-  {
-    path: '/info_more_sex',
-    name: 'Info_more_sex',
-    component: Info_more_sex
-  },
-  {
-    path: '/info_more_add',
-    name: 'Info_more_add',
-    component: Info_more_add
-  },
-  {
-    path: '/info_more',
-    name: 'Info_more',
-    component: Info_more
-  },
-  {
-    path: '/info',
-    name: 'Info',
-    component: Info
-  },
-  {
-    path: '/mine',
-    name: 'Mine',
-    component: Mine
-  },
-  {
-<<<<<<< HEAD
     path: '/detail',
     name: 'Detail',
     component: Detail
@@ -108,11 +29,6 @@ const routes = [
     component: Home
   },
   {
-    path: '/recommend',
-    name: 'Recommend',
-    component: Recommend
-  },
-=======
     path: '/about',
     name: 'About',
     // route level code-splitting
@@ -120,7 +36,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
->>>>>>> 6cdee9fe56b058414ac646fb2e969e46c407a98b
 ]
 
 const router = new VueRouter({
@@ -130,7 +45,3 @@ const router = new VueRouter({
 })
 
 export default router
-<<<<<<< HEAD
-=======
-
->>>>>>> 6cdee9fe56b058414ac646fb2e969e46c407a98b
