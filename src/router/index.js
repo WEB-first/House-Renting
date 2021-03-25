6cdee9fe56b058414ac646fb2e969e46c407a98b
xiaomai <2099138437@qq.com>
2021-03-25 10:16:24 +0800

    测试

--- src/router/index.js
+++ src/router/index.js
@@ -1,57 +1,10 @@
-<<<<<<< HEAD
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Detail from '../views/Detail.vue'
-import Article from '../views/Article.vue'
-import Recommend from '../views/Recommend.vue'
-
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/article',
-    name: 'Article',
-    component: Article
-  },
-  {
-    path: '/detail',
-    name: 'Detail',
-    component: Detail
-  },
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/recommend',
-    name: 'Recommend',
-    component: Recommend
-  },
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-export default router
-=======
-<<<<<<< HEAD
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Mine from '../views/Mine.vue'
+import Detail from '../views/Detail.vue'
+import Article from '../views/Article.vue'
+import Recommend from '../views/Recommend.vue'
 import Info from '../components/Info.vue'
 import Info_more from '../components/Info_more.vue'
 import Info_more_add from '../components/Info_more_add.vue'
@@ -73,6 +26,21 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/article',
+    name: 'Article',
+    component: Article
+  },
+  {
+    path: '/detail',
+    name: 'Detail',
+    component: Detail
+  },
+  {
+    path: '/recommend',
+    name: 'Recommend',
+    component: Recommend
   },
   {
     path: '/info_uname',
@@ -146,47 +114,4 @@ const router = new VueRouter({
 })
 
 export default router
-=======
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Detail from '../views/Detail.vue'
-import Article from '../views/Article.vue'
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/article',
-    name: 'Article',
-    component: Article
-  },
-  {
-    path: '/detail',
-    name: 'Detail',
-    component: Detail
-  },
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-export default router
->>>>>>> 594b0018069db21474cd0d6ab76937ae6e480ed1
->>>>>>> 4d64a3944df67b81d93f5d1d1d9fe4f8531ec2c5
--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
     <div class="mine">
         <!-- 顶部 -->
@@ -96,82 +95,4 @@ export default {
     methods:{
     }
 }
-=======
-<template>
-    <div class="mine">
-        <div id="top">
-            <div id="avatar"><img src="../assets/logo.png" alt="../assets/logo.png"></div>
-            <div>
-                <p>张三</p>
-                <!-- <p>快去登录吧</p> -->
-                <p>个人中心</p>
-            </div>
-        </div>
-        <div id="bottom">
-            <mt-cell title="关注">
-                <img slot="icon" src="../assets/logo.png" width="24" height="24">
-            </mt-cell>
-            <mt-cell title="足迹">
-                <img slot="icon" src="../assets/logo.png" width="24" height="24">
-            </mt-cell>
-            <mt-cell title="设置">
-                <img slot="icon" src="../assets/logo.png" width="24" height="24">
-            </mt-cell>
-            <mt-cell title="我的问答">
-                <img slot="icon" src="../assets/logo.png" width="24" height="24">
-            </mt-cell>
-            <mt-cell title="文章列表">
-                <img slot="icon" src="../assets/logo.png" width="24" height="24">
-            </mt-cell>
-        </div>
-    </div>
-</template>
-<style>
-*{box-sizing: border-box}
-.mine{
-    height: 100vh;
-    background:white;
-}
-.mine #top{
-    width: 100%;
-    height:200px;
-    display: flex;
-    background:cadetblue;
-    padding-top:90px;
-}
-.mine #avatar{
-    height: 80px;
-    width: 80px;
-    border-radius: 10%;
-    box-shadow: 0px 0px 4px 0px rgb(5, 99, 94);
-    margin-right: 15px;
-    margin-left: 30px;
-}
-.mine #avatar>img{
-    width: 100%;
-}
-.mine #avatar+div{
-    height: 90px;
-}
-.mine #avatar+div>p{
-    font-size:18px;
-    color:white;
-    margin-top:16px;
-}
-.mine .mint-cell-wrapper{
-    padding: 0px 15px !important;
-    background-size: 100% 1px;
-}
-</style>
-<script>
-export default {
-    name:"Mine",
-    data(){
-        return{
-        }
-    },
-    methods:{
-    }
-}
->>>>>>> 594b0018069db21474cd0d6ab76937ae6e480ed1
 </script>
