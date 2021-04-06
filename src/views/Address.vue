b4b718b7ef3fcc70a95b076a6d5cfddcd60d407c
LiHaohua <870700726@qq.com>
2021-04-06 09:59:27 +0800

    功能修改

--- src/views/Address.vue
+++ src/views/Address.vue
@@ -1,30 +1,105 @@
 <template>
 <div>
-  <mt-search
-    v-model="value"
-    cancel-text="取消"
-    placeholder="搜索">
-  </mt-search>
-  <mt-cell
-  title="西安"
-  to="/"
-  is-link>
-</mt-cell>
+<div class="city-list-container">
+  <div class="city-title">定位城市</div>
+  <div class="city-inline">
+    <router-link to="/" class="location-city">西安市</router-link>
+  </div>
+  </div>
+  <div class="city-list">
+    <div class="city-left">
+      <div style="height:100vh">
+        <div class="city-group-item" v-for="(item,i) of mapList" :key="i">
+            <!-- 为城市组的标题添加唯一id
+                 为了动态滚动到相应的位置 -->
+            <div class="city-group-title">{{i}}</div>
+            <div class="city-fullname" v-for="(cityObj,i) of item" :key="i">
+              <router-link style="color:#333" to="/">{{cityObj.fullName}}</router-link>
+        </div>    
+      </div>
+    </div>
+    <div class="city-right">
+      <div class="py-item" style="padding:0;height:20px;" v-for="(item,i) of letters" :key="i">
+        <div>{{item}}</div>
+      </div>       
+    </div>
+  </div>
+</div>
 </div>
 </template>
 <script>
+import map from '../assets/map'
 export default {
-  data(){
-    return{
-      value:''
-    }
-  }
-}
+  data() {
+    return {
+      value: "",
+      mapList:map,
+      letters:['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    };
+  },
+};
 </script>
 
 
 <style scoped>
-.mint-search{
-  height: 7vh;
+
+body {
+  background-color: #ebebeb;
+ 
+}
+.city-list-container {
+  font-size: 14px;
+  color: #333;
+}
+.city-title {
+  padding-left: 15px;
+  line-height: 30px;
+}
+.city-inline {
+  background-color: #f5f5f5;
+  padding: 10px;
+}
+.location-city {
+  display: inline-block;
+  width: auto;
+  padding: 0 20px;
+  background: #fff;
+  height: 34px;
+  line-height: 34px;
+  color: #333;
+}
+.city-list {
+  height: 100%;
+  background: #fff;
+  display: flex;
+}
+.city-left {
+  flex: 1;
+}
+.city-right {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+.city-right .py-item {
+  text-align: center;
+  padding: 4px 0;
+}
+.city-group-item {
+  margin: 10px 0;
+}
+.city-group-title {
+  background-color: #ebebeb;
+  padding: 10px;
+}
+.city-fullname {
+  color: #333;
+  padding: 15px 20px;
+  border-bottom: 1px dashed #ccc;
+
 }
 </style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,12 +1,12 @@
 <template>
 <div>
   <mt-search v-model="value"></mt-search>
-  <div  v-if="result.length!==0" style="position:absolute;z-index:10">
-    <div v-for="(item,i) of result" :key="i">
-  <router-link :to="`/detail?id=`+`${item.aid}`">{{item.a_area}}</router-link>
+  <div v-if="result.length!==0" style="position:absolute;z-index:10;width: 100%;">
+    <div v-for="(item,i) of result" :key="i" class="city-fullname">
+  <router-link style="color:#333" :to="`/detail?id=`+`${item.aid}`">{{item.title}}</router-link>
     </div>
   </div>
-  
+  <my-footer></my-footer>
 </div>
 </template>
 <script>
@@ -33,5 +33,10 @@ export default {
 .mint-search{
   height: 8vh;
 }
-
+.city-fullname {
+  padding: 15px 20px;
+  color: #000;
+  background-color: #eee;
+  border-bottom: 1px dashed #ccc;
+}
 </style>
